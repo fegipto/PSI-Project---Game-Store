081c16551ae414e2b3657cfeb8e4089e07cf13e5
fc56272 <fc56272@alunos.ciencias.ulisboa.pt>
2023-04-30 17:06:37 +0100

    Revert "inicial implementation of user's login"
    This reverts commit fb17a6f4e872c6e0d9299173ad970e8bea44752d

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
+
+const ItemSchema = require("../models/Item");
 
 const UserSchema = new Schema({
   id: { type: String, required: true, maxLength: 100 },
@@ -9,11 +10,6 @@ const UserSchema = new Schema({
   library:   {type: [Schema.Types.ObjectId],ref: 'Item'},
   followers: {type: [Schema.Types.ObjectId],ref: 'User'},
   following: {type: [Schema.Types.ObjectId],ref: 'User'},
-});
-
-// Virtual for user's URL
-UserSchema.virtual("url").get(function () {
-  return `/user/${this._id}`;
 });
 
 module.exports = mongoose.model("User", UserSchema);
