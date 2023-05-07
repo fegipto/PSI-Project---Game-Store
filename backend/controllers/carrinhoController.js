37d9a06a7216ed793a6a4b4f0d8c66a506de077d
Martim Paraíba <martim.paraiba@gmail.com>
2023-05-07 23:31:44 +0100

    Placeholder for update_cart function

--- backend/controllers/carrinhoController.js
+++ backend/controllers/carrinhoController.js
@@ -5,3 +5,15 @@ exports.find_cart =  async (req, res) => {
     const result = await query.findOne();
     res.send(result);
   };
+
+exports.update_cart = async (req, res) => {
+  const res = await cart.where('_id', req.params.carrinho.id).findOne();
+
+  if (res) {
+    console.log('Update cart');
+    return res.json("ok");
+  }
+
+  console.log('Do not update cart');
+  return res.json("not_ok");
+  };
