e23992e80804c6c6c8a4018a5e18bc26432f8b73
Martim Paraíba <martim.paraiba@gmail.com>
2023-05-07 23:28:36 +0100

    Switch to dev DB

--- backend/app.js
+++ backend/app.js
@@ -16,8 +16,8 @@ const mongoose = require("mongoose");
 mongoose instanceof mongoose.Mongoose; // true
 mongoose.set('strictQuery', false);
 const m = new mongoose.Mongoose();
-//const mongoDB = "mongodb+srv://tiagomg7fernandes:mBTQcpcGgtJeLuDj@locallibrary.kxp3fjt.mongodb.net/my_database?retryWrites=true&w=majority";
-const mongoDB = "mongodb://psi031:psi031@localhost:27017/psi031?retryWrites=true&authSource=psi031";
+const mongoDB = "mongodb+srv://tiagomg7fernandes:mBTQcpcGgtJeLuDj@locallibrary.kxp3fjt.mongodb.net/my_database?retryWrites=true&w=majority";
+// const mongoDB = "mongodb://psi031:psi031@localhost:27017/psi031?retryWrites=true&authSource=psi031";
 
 
 main().catch(err => console.log(err));
