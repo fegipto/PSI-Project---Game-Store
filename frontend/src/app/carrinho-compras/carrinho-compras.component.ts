97859750e75e2ac4e6b240607bb79ea96caf7159
Martim Paraíba <martim.paraiba@gmail.com>
2023-05-11 18:47:25 +0100

    Better implementation of quantity per item on Cart

--- frontend/src/app/carrinho-compras/carrinho-compras.component.ts
+++ frontend/src/app/carrinho-compras/carrinho-compras.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Item } from '../item';
 import { ItemService } from '../service/item.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-carrinho-compras',
@@ -8,53 +9,85 @@ import { ItemService } from '../service/item.service';
   styleUrls: ['./carrinho-compras.component.css'],
 })
 export class CarrinhoComprasComponent {
-getTotalPrice() {
-  let sum = 0;
-  this.items.forEach((item) => {
-    sum += item.preco * this.quantity;
-  });
-  return sum.toFixed(2);
-}
-
   /**
    *
    */
-  constructor(private itemService: ItemService) {
-    
-  }
+  constructor(private itemService: ItemService) {}
 
-  
   items: Item[] = [];
 
-  quantity = 1;
+  quantities: Map<number, number> = new Map<number, number>();
 
-  ngOnInit() { 
-    this.itemService.searchItems("ite").subscribe( items => {  
-      this.items = items;
+  async ngOnInit() {
+
+    await this.getItems();
+
+    this.items.forEach((item) => {
+      if (!this.quantities.has(item.id)) {
+        this.quantities.set(item.id, 1);
+      }
     });
+
   }
 
-  removeItem(itemID: Number) {
+  async getItems() {
+    // TODO: temporary item loading
+    this.items = await firstValueFrom(this.itemService.searchItems('ite'));
+  }
+
+  removeItem(itemID: number) {
     if (this.items) {
-      this.items.splice(this.items.findIndex( (item) => {
-        if (item.id === itemID) {
-          return true;
-        }
-        return false;
-      } ), 1);
+      this.items.splice(
+        this.items.findIndex((item) => {
+          if (item.id === itemID) {
+            return true;
+          }
+          return false;
+        }),
+        1
+      );
     }
   }
 
-
-  increaseAmount(itemID: Number) {
-    this.quantity++;
+  increaseAmount(itemID: number) {
+    const curr = this.quantities.get(itemID);
+    if (curr) {
+      this.quantities.set(itemID, curr + 1);
+    }
   }
 
-  decreaseAmount(itemID: Number) {
-    if (this.quantity > 1) {
-      this.quantity--;
+  decreaseAmount(itemID: number) {
+    const curr = this.quantities.get(itemID);
+    if (curr && curr > 1) {
+      this.quantities.set(itemID, curr - 1);
     } else {
-      // this.removeItem();
+      this.removeItem(itemID);
     }
+  }
+
+  getTotalPrice() {
+    let sum = 0;
+    this.items.forEach((item) => {
+      const itemQuantity = this.quantities.get(item.id);
+      if (itemQuantity) {
+        sum += item.preco * itemQuantity;
+      }
+    });
+    return sum.toFixed(2);
+  }
+
+  getItemPrice(itemID: number) {
+    const item = this.items.find((item) => {
+      if (item.id === itemID) {
+        return true;
+      }
+      return false;
+    });
+    const itemQuantity = this.quantities.get(itemID);
+
+    if (itemQuantity && item) {
+      return (item.preco * itemQuantity).toFixed(2);
+    }
+    return undefined;
   }
 }
