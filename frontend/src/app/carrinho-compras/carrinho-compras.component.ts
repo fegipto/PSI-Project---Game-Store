20ac5c57fc380fa599889f17bc21080100ba6ca8
Martim Paraíba <martim.paraiba@gmail.com>
2023-05-11 21:51:05 +0100

    Shopping-cart component implemented

--- frontend/src/app/carrinho-compras/carrinho-compras.component.ts
+++ frontend/src/app/carrinho-compras/carrinho-compras.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Item } from '../item';
 import { ItemService } from '../service/item.service';
 import { firstValueFrom } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-carrinho-compras',
@@ -9,30 +10,47 @@ import { firstValueFrom } from 'rxjs';
   styleUrls: ['./carrinho-compras.component.css'],
 })
 export class CarrinhoComprasComponent {
-  /**
-   *
-   */
-  constructor(private itemService: ItemService) {}
+  constructor(
+    private itemService: ItemService,
+    private cookieService: CookieService
+  ) {}
 
   items: Item[] = [];
 
   quantities: Map<number, number> = new Map<number, number>();
 
   async ngOnInit() {
-
-    await this.getItems();
+    /* await this.getItems();
 
     this.items.forEach((item) => {
       if (!this.quantities.has(item.id)) {
         this.quantities.set(item.id, 1);
       }
-    });
-
+    }); */
+    this.loadCart();
   }
+
+  /* ngOnDestroy() {
+    this.saveCart();
+  } */
 
   async getItems() {
     // TODO: temporary item loading
     this.items = await firstValueFrom(this.itemService.searchItems('ite'));
+  }
+
+  async addItem(itemID: number) {
+    const item: Item = await firstValueFrom(this.itemService.getItem(itemID));
+
+    if (item) {
+      if (this.quantities.has(item.id)) {
+        this.increaseAmount(item.id);
+        return;
+      }
+      this.items.push(item);
+      this.quantities.set(item.id, 1);
+    }
+    this.saveCart();
   }
 
   removeItem(itemID: number) {
@@ -46,7 +64,9 @@ export class CarrinhoComprasComponent {
         }),
         1
       );
+      this.quantities.delete(itemID);
     }
+    this.saveCart();
   }
 
   increaseAmount(itemID: number) {
@@ -54,6 +74,7 @@ export class CarrinhoComprasComponent {
     if (curr) {
       this.quantities.set(itemID, curr + 1);
     }
+    this.saveCart();
   }
 
   decreaseAmount(itemID: number) {
@@ -63,6 +84,7 @@ export class CarrinhoComprasComponent {
     } else {
       this.removeItem(itemID);
     }
+    this.saveCart();
   }
 
   getTotalPrice() {
@@ -89,5 +111,31 @@ export class CarrinhoComprasComponent {
       return (item.preco * itemQuantity).toFixed(2);
     }
     return undefined;
+  }
+
+  saveCart() {
+    const quantitiesObject = Array.from(this.quantities);
+    this.cookieService.set(
+      'shoppingCartQuantities',
+      JSON.stringify(quantitiesObject)
+    );
+    this.items.forEach((item) => {
+      item.imagens = [];
+    });
+    this.cookieService.set('shoppingCartItems', JSON.stringify(this.items));
+  }
+
+  loadCart() {
+    try {
+      const quantitiesArray = JSON.parse(
+        this.cookieService.get('shoppingCartQuantities')
+      );
+      this.quantities = new Map(quantitiesArray);
+      this.items = JSON.parse(this.cookieService.get('shoppingCartItems'));
+    } catch (error) {
+      this.quantities = new Map();
+      this.items = [];
+      return;
+    }
   }
 }
