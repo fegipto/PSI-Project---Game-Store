b47a37358d427dbd9267bb44a2423c28655c265c
Martim Paraíba <martim.paraiba@gmail.com>
2023-05-11 17:58:56 +0100

    Added placeholder for an empty cart Simplistic remove item from cart function implemented Simplistic total price calculus implemented

--- frontend/src/app/carrinho-compras/carrinho-compras.component.ts
+++ frontend/src/app/carrinho-compras/carrinho-compras.component.ts
@@ -1,26 +1,60 @@
 import { Component } from '@angular/core';
-
-declare const Array: any;
+import { Item } from '../item';
+import { ItemService } from '../service/item.service';
 
 @Component({
   selector: 'app-carrinho-compras',
   templateUrl: './carrinho-compras.component.html',
-  styleUrls: ['./carrinho-compras.component.css']
+  styleUrls: ['./carrinho-compras.component.css'],
 })
 export class CarrinhoComprasComponent {
-Array(arg0: number): any {
-  var res = new Array();
-  for (let index = 0; index < arg0; index++) {
-    res.push(index);
-  }
-  return res;
+getTotalPrice() {
+  let sum = 0;
+  this.items.forEach((item) => {
+    sum += item.preco * this.quantity;
+  });
+  return sum.toFixed(2);
 }
 
   /**
    *
    */
-  constructor() {
+  constructor(private itemService: ItemService) {
     
   }
 
+  
+  items: Item[] = [];
+
+  quantity = 1;
+
+  ngOnInit() { 
+    this.itemService.searchItems("ite").subscribe( items => {  
+      this.items = items;
+    });
+  }
+
+  removeItem(itemID: Number) {
+    if (this.items) {
+      this.items.splice(this.items.findIndex( (item) => {
+        if (item.id === itemID) {
+          return true;
+        }
+        return false;
+      } ), 1);
+    }
+  }
+
+
+  increaseAmount(itemID: Number) {
+    this.quantity++;
+  }
+
+  decreaseAmount(itemID: Number) {
+    if (this.quantity > 1) {
+      this.quantity--;
+    } else {
+      // this.removeItem();
+    }
+  }
 }
