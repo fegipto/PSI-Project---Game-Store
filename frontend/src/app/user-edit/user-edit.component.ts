5f7ac91de38e816c8b20874f2cfe56b007157d82
fc56272 <fc56272@alunos.fc.ul.pt>
2023-05-15 23:31:33 +0100

    Get used usernames and alert if already used

--- frontend/src/app/user-edit/user-edit.component.ts
+++ frontend/src/app/user-edit/user-edit.component.ts
@@ -1,18 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
   styleUrls: ['./user-edit.component.css']
 })
-export class UserEditComponent {
+export class UserEditComponent implements OnInit {
 
   constructor(private location:  Location) { }
+
+  name = '';
+  usernames: String[] = [];
 
   goBack(): void {
     this.location.back();
   }
 
+  ngOnInit(): void {
+    this.getUsernames();
+  }
 
+  getUsernames(): void {
+
+  }
+
+  onSubmit(form: NgForm): void {
+    this.name = form.value.name.trim();
+
+    if(this.usernames.includes(this.name)) {
+      alert('Username already being used. Please try a different one!');
+      return;
+    }
+
+    //this.userLoginService.addUser({ name: this.username, password: this.password } as User).subscribe();
+    alert('Profile successfully updated.')
+    window.location.assign('dashboard');
+  }
 }
